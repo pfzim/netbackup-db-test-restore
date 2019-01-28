c3b9380132aef9f88e44d14b05e51605beb65db4
Dmitry V. Zimin <pfzim@mail.ru>
2019-01-28 10:51:00 +0300

    Update

--- scripts/NetBackupFillDB.ps1
+++ scripts/NetBackupFillDB.ps1
@@ -31,26 +31,7 @@ foreach($j in $json)
 
     try
     {
-        $cmd.CommandText = 'SELECT COUNT(*) FROM nbt_images AS m WHERE m.`backupid` = "{0}"' -f $j.backupid
-        $last_id = [int] $cmd.ExecuteScalar()
-        if($last_id -eq 0)
-        {
-            $cmd.CommandText = 'INSERT INTO nbt_images (`policy_name`, `sched_label`, `client_name`, `backup_time`, `expiration`, `backupid`, `ss_name`, `media_list`) VALUES ("{0}", "{1}", "{2}", "{3}", "{4}", "{5}", "{6}", "{7}")' -f $j.policy_name, $j.sched_label, $j.client_name, $j.backup_time, $j.expiration, $j.backupid, $j.ss_name, ($media_list -join ",")
-            $cmd.CommandText
-            $cmd.ExecuteNonQuery() | Out-Null
-            $cmd.CommandText = "SELECT LAST_INSERT_ID()"
-            $last_id = [int] $cmd.ExecuteScalar()
-        }
-    }
-    catch
-    {
-        Write-Host -ForegroundColor Red $_.Exception.Message
-    }
-
-    $date = (((Get-Date "1970-01-01 00:00:00.000Z") + ([TimeSpan]::FromSeconds($j.backup_time)))).ToString("MM/dd/yyyy HH:mm")
-
-    try
-    {
+        #$date = (((Get-Date "1970-01-01 00:00:00.000Z") + ([TimeSpan]::FromSeconds($j.backup_time)))).ToString("MM/dd/yyyy HH:mm")
         #$images = (& 'C:\Program Files\Veritas\NetBackup\bin\bplist.exe' -s $date -e $date -C $j.client_name -k $j.policy_name -t 15 -R \) | Sort-Object -Unique
         $image_data = (& 'C:\Program Files\Veritas\NetBackup\bin\admincmd\bpflist.exe' -rl 1 -backupid $j.backupid -json)
         $image = $image_data | ConvertFrom-Json
@@ -68,14 +49,14 @@ foreach($j in $json)
 		{
 		    $stripes = [int] $data[7].Substring(5, 3)
 		    $nbimage = $f.path.Substring(1, $f.path.Length - 1)
-
-            try
+            
+			try
             {
-                $cmd.CommandText = 'SELECT COUNT(*) FROM nbt_files AS m WHERE m.`name` = "{0}"' -f $nbimage
+                $cmd.CommandText = 'SELECT COUNT(*) FROM nbt_images AS m WHERE m.`backupid` = "{0}" OR m.`nbimage` = {1}' -f $j.backupid, $nbimage
                 $exist = [int] $cmd.ExecuteScalar()
                 if($exist -eq 0)
                 {
-                    $cmd.CommandText = 'INSERT INTO nbt_files (`pid`, `name`, `date2`, `db`, `stripes`) VALUES ("{0}", "{1}", "{2}", "{3}", "{4}")' -f $last_id, $nbimage, $data[8], $data[4], $stripes
+                    $cmd.CommandText = 'INSERT INTO nbt_images (`policy_name`, `sched_label`, `client_name`, `backup_time`, `expiration`, `backupid`, `ss_name`, `media_list`, `nbimage`, `date2`, `db`, `stripes`) VALUES ("{0}", "{1}", "{2}", "{3}", "{4}", "{5}", "{6}", "{7}", "{8}", "{9}", "{10}", "{11}", "{4}")' -f $j.policy_name, $j.sched_label, $j.client_name, $j.backup_time, $j.expiration, $j.backupid, $j.ss_name, ($media_list -join ","), $nbimage, $data[8], $data[4], $stripes
                     $cmd.CommandText
                     #$cmd = new-object System.Data.Odbc.OdbcCommand($query,$conn)
                     $cmd.ExecuteNonQuery() | Out-Null
@@ -83,7 +64,7 @@ foreach($j in $json)
                 }
                 else
                 {
-                    Write-Host -ForegroundColor Red ("ERROR Exist {1} : {0}" -f $nbimage, $j.backupid)
+                    Write-Host -ForegroundColor Red ("SKIPPED existing backup {1} : {0}" -f $nbimage, $j.backupid)
                 }
             }
             catch
@@ -97,4 +78,5 @@ foreach($j in $json)
 $conn.close()
 
 
-# UPDATE nbt_files SET `date` = CONCAT(SUBSTRING(`date2`, 1, 4), '-', SUBSTRING(`date2`, 5, 2), '-', SUBSTRING(`date2`, 7, 2), ' ', SUBSTRING(`date2`, 9, 2), ':', SUBSTRING(`date2`, 11, 2), ':', SUBSTRING(`date2`, 13, 2));
+
+# UPDATE nbt_files SET `date2` = CONCAT(SUBSTRING(`date`, 1, 4), '-', SUBSTRING(`date`, 5, 2), '-', SUBSTRING(`date`, 7, 2), ' ', SUBSTRING(`date`, 9, 2), ':', SUBSTRING(`date`, 11, 2), ':', SUBSTRING(`date`, 13, 2));
